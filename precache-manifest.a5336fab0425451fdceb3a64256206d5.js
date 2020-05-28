a3801f135a288bf205a757ff1d75cab01d18e1f1
unicorn230 <fedzhaga10@gmail.com>
2020-05-28 18:45:37 +0300

    Updates

---
+++ precache-manifest.a5336fab0425451fdceb3a64256206d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5473902934e44c9108a81f1b9d20b39",
+    "url": "/erfahren/index.html"
+  },
+  {
+    "revision": "aa9abfe1950c2c4ba159",
+    "url": "/erfahren/static/css/main.ade30b0b.chunk.css"
+  },
+  {
+    "revision": "1832a756a8394892e42f",
+    "url": "/erfahren/static/js/2.ee4ed3d0.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/erfahren/static/js/2.ee4ed3d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9abfe1950c2c4ba159",
+    "url": "/erfahren/static/js/main.22d345a9.chunk.js"
+  },
+  {
+    "revision": "68096c6b11aa30dfd83c",
+    "url": "/erfahren/static/js/runtime-main.ee211527.js"
+  },
+  {
+    "revision": "c2c919490a8cadc16b6f1a16fe5c2269",
+    "url": "/erfahren/static/media/close.c2c91949.svg"
+  },
+  {
+    "revision": "b6757aed1c8957eaba7bdfa8ea21b902",
+    "url": "/erfahren/static/media/menu.b6757aed.svg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/erfahren/static/media/oval.be00fc4a.svg"
+  }
+]);
